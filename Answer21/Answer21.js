2a43a63844902690666c314aba5995a684a84740
nastaran-shafie <nastaran.sh33@gmail.com>
2022-12-11 12:18:15 +0330

    feat:create area function

---
+++ Answer21/Answer21.js
@@ -0,0 +1,6 @@
+function rectangleArea(height, width) {
+  return height * width;
+}
+
+console.log(rectangleArea(10, 20)); //The area of rectangle is 200
+console.log(rectangleArea(30, 30)); //The area of rectangle is 900
