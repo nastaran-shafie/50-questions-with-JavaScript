9d537bc70557770e4c5f77410a9956feb721e6c5
nastaran-shafie <nastaran.sh33@gmail.com>
2022-12-11 19:14:27 +0330

    feat:create function

---
+++ Answer49/Answer49.js
@@ -0,0 +1,8 @@
+let myColor = ["Red", "Green", "White", "Black"];
+
+function joinAll(myColor) {
+  let str = myColor.join(",");
+  console.log(str);
+}
+
+joinAll(myColor); // "Red,Green,White,Black"
